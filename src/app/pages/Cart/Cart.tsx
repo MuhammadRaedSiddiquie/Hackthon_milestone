3ed86fef7542e3b1f81cb9bff8b2b5a937bd2618
MuhammadRaedSiddiquie <bhairaed636@gmail.com>
2024-12-07 17:34:01 +0500

    first commit

---
+++ src/app/pages/Cart/Cart.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cart() {
+  return (
+    <section>
+        
+    </section>
+  )
+}
+
+export default Cart
---
+++ src/app/pages/Checkout/Checkout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Checkout() {
+  return (
+    <section>
+        
+    </section>
+  )
+}
+
+export default Checkout
